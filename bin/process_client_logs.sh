99975b399f7a68a0f9ec45d54a97b74e09e05f13
UtkarshKr007 <kumar375@morris.umn.edu>
2019-09-05 11:52:53 -0500

    create failed_login_data.txt with no filters applied

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 dir=$1
 cd $dir
-for file in ./*.tgz
+for file in *.tgz
 do
-	echo $file
-	filename=$(basename -- $file)
-	echo $filename
-	echo $(basename $filename .tgz)
-	mkdir $(basename $filename .tgz)
-	tar -zxf $file -C $(basename $filename .tgz)
+	cat */var/log/secure* > failed_login_data.txt
 done	
