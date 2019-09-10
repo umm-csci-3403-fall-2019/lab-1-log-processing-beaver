146eca47ed7b72faceb035967f1c689d7b15d84b
UtkarshKr007 <kumar375@morris.umn.edu>
2019-09-10 10:28:53 -0500

    We have successfully ran test bats for process_client_logs and created create_username_dist and made it executable.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 dir=$1
 cd $dir
-for file in *.tgz
-do
-       cat */var/log/secure* | sed -E -n 's/(^[^:]+).+: Failed password for (invalid user )?([a-zA-Z]*) from ([0-9.]*) port [0-9]* ssh2/\1 \3 \4/p' > failed_login_data.txt
-done	
+cat var/log/secure* | sed -E -n 's/(^[^:]+).+: Failed password for (invalid user )?([a-zA-Z0-9_-]*) from ([0-9.]*) port [0-9]* ssh2/\1 \3 \4/p' > failed_login_data.txt
