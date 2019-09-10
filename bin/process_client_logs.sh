c500fe1a268a7425d263ae25ffb0ad56ecc703fe
UtkarshKr007 <kumar375@morris.umn.edu>
2019-09-09 20:50:04 -0500

    add regex for process_client_logs.sh

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,5 +3,5 @@ dir=$1
 cd $dir
 for file in *.tgz
 do
-	cat */var/log/secure* > failed_login_data.txt
+       cat */var/log/secure* | sed -E -n 's/(^[^:]+).+: Failed password for (invalid user )?([a-zA-Z]*) from ([0-9.]*) port [0-9]* ssh2/\1 \3 \4/p' > failed_login_data.txt
 done	
