9527fd74abc0e53155dfd042e9d05fe697f372d7
Utkarsh Kumar <kumar375@morris.umn.edu>
2019-09-12 10:35:06 -0500

    We finished create_hours and passed bats test and created create_country_dist.

---
+++ bin/create_hours_dist.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#This captures starting directory using pwd.
+cDir=$(pwd)
+
+#This initializes the directory in the argument as a variable.
+dir=$1
+
+#This goes into the directory.
+cd $dir
+
+#cat grabs the failed login data from every folder. sed then captures the hour and sort organizes the data numerically.
+#uniq counts the number of occurences for a hour. The last sed adds the appropriate java script lines.
+cat */failed_login_data.txt \
+	| sed -E -n 's/[A-Za-z]* [0-9]* ([0-9]*) [A-Za-z0-9_-]* [0-9.]*/\1/p' \
+        | sort \
+        | uniq -c \
+        | sed -E -n "s/[ ]*([0-9]*) ([0-9]*)/data.addRow(['\2', \1]);/p" \
+        > hours_dist.txt
+
+#this goes back into the top level directory.
+cd $cDir
+
+#this uses wrap_contents to wrap the header and footer with the javascript. $dir is the specified location where it is at.
+#Then it puts the wrap_contents in $dir/hours_dist.html.
+./bin/wrap_contents.sh $dir/hours_dist.txt html_components/hours_dist $dir/hours_dist.html
+
