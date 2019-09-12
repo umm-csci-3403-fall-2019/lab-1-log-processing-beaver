1a77940c0136cf79b27d9aebf82547ff5fc91411
Utkarsh Kumar <kumar375@morris.umn.edu>
2019-09-12 10:13:40 -0500

    We updated the comments located in each of the .sh files.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
+
+#This captures starting directory using pwd.
 cDir=$(pwd)
+
+#This initializes the directory in the argument as a variable.
 dir=$1
+
+#This goes into the directory.
 cd $dir
+
+#cat grabs the failed login data from every folder. sed then captures the username and sort organizes the data alphabetically.
+#uniq counts the number of occurences for a username. The last sed adds the appropriate java script lines.
 cat */failed_login_data.txt \
 	| sed -E -n 's/[A-Za-z]* [0-9]* [0-9]* ([A-Za-z0-9_-]*) [0-9.]*/\1/p' \
 	| sort \
 	| uniq -c \
 	| sed -E -n "s/[ ]*([0-9]*) ([A-Za-z0-9_-]*)/data.addRow(['\2', \1]);/p" \
 	> username_dist.txt
+
+#this goes back into the top level directory.
 cd $cDir
+
+#this uses wrap_contents to wrap the header and footer with the javascript. $dir is the specified location where it is at.
+#Then it puts the wrap_contents in $dir/username_dist.html.
 ./bin/wrap_contents.sh $dir/username_dist.txt html_components/username_dist $dir/username_dist.html
 
--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+#takes a directory and sets it as dir variable
 dir=$1
+#go into directory
 cd $dir
+#cat combines the text and then uses sed to select only the relevant text parts. Then outputs to failed_login_data.txt.
 cat var/log/secure* | sed -E -n 's/(^[^:]+).+: Failed password for (invalid user )?([a-zA-Z0-9_-]*) from ([0-9.]*) port [0-9]* ssh2/\1 \3 \4/p' > failed_login_data.txt
